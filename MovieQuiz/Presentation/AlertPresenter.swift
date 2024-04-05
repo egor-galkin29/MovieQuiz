ae36031b155a5dfa948a325b43de06838da3dc8b
egor <egorgalkin290907@gmail.com>
2024-04-05 00:35:51 -0400

    .

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class AlertPresenter {
+    private func show(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in // слабая ссылка на self
+            guard let self = self else { return } // разворачиваем слабую ссылку
+
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+
+            questionFactory?.requestNextQuestion()
+        }
+        
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+}
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -93,25 +93,25 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
         }
     }
 
-    private func show(quiz result: QuizResultsViewModel) {
-        let alert = UIAlertController(
-            title: result.title,
-            message: result.text,
-            preferredStyle: .alert)
+    //private func show(quiz result: QuizResultsViewModel) {
+       // let alert = UIAlertController(
+        //    title: result.title,
+            //message: result.text,
+            //preferredStyle: .alert)
+        //
+        //let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in // слабая ссылка на self
+         //   guard let self = self else { return } // разворачиваем слабую ссылку
+//
+     //       self.currentQuestionIndex = 0
+      //      self.correctAnswers = 0
+//
+       //     questionFactory?.requestNextQuestion()
+       // }
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in // слабая ссылка на self
-            guard let self = self else { return } // разворачиваем слабую ссылку
-
-            self.currentQuestionIndex = 0
-            self.correctAnswers = 0
-
-            questionFactory?.requestNextQuestion()
-        }
+       // alert.addAction(action)
         
-        alert.addAction(action)
-        
-        self.present(alert, animated: true, completion: nil)
-    }
+     //   self.present(alert, animated: true, completion: nil)
+    //}
     
     
     @IBAction private func noButtonClicked(_ sender: Any) {
